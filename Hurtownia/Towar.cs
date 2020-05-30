ffbd659ae0d6f61fad1ef97cb5416a6c802ad4db
WayOver <wisnia_198@wp.pl>
2020-05-30 10:57:08 +0200

    Dodanie

--- Hurtownia/Towar.cs
+++ Hurtownia/Towar.cs
@@ -8,5 +8,22 @@ namespace Hurtownia
 {
     class Towar
     {
+
+        public string nazwa;
+        public float cena;
+
+        public Towar(string naz,float ce)
+        {
+            nazwa = naz;
+            cena = ce;
+        }
+
+        public Towar(string naz)
+        {
+            nazwa = naz;
+            cena = 0;
+        }
+
+
     }
 }
